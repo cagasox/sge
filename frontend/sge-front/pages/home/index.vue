f309055b536522bc714f7b2e744df2c7132b2e1a
cagasox <pedfor633@gmail.com>
2024-03-19 20:20:25 -0300

    hbgv

---
+++ frontend/sge-front/pages/home/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const {signOut} = useAuth();
+
+definePageMeta({
+    middleware:'auth'
+})
+
+const submitLogout = ()=> {
+    console.log("try to login whith credentials below:");
+        signOut({redirect:false}).then(()=>{
+                console.log("successefully logout!")
+                navigateTo('/');
+            })
+            
+        .catch((error)=>{
+            console.log("error when trying to login", error)
+        })
+}
+</script>
+
+<template>
+    <div>
+        <h1> Minha Tela Home!!</h1>
+        <button @click="submitLogout" class=""customButton>Sair</button>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
